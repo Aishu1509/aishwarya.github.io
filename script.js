30b02e455bb7cb21bbb4bfb9669ca9d42dd5268e
Aishu1509 <139570631+Aishu1509@users.noreply.github.com>
2024-04-16 18:58:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/*========== typing animation ===========*/
+var typed = new Typed(".typing",{
+    strings:["","Student","Web Designer","Artist"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+  })
